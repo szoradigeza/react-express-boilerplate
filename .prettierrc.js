c5919eea9e0bfa8a19af6b2b1a934ab18f866a8d
Geza Szoradi <geza.szilard@vodafone.com>
2023-07-04 14:49:55 +0200

    Setup BE docker for prod build

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,10 @@
 module.exports = {
-  semi: true,
-  trailingComma: 'all',
-  singleQuote: true,
-  printWidth: 90,
-  tabWidth: 2,
-  endOfLine: 'auto',
+     semi: true,
+     trailingComma: 'all',
+     singleQuote: true,
+     printWidth: 90,
+     tabWidth: 2,
+     endOfLine: 'auto',
 };
 
 // {
